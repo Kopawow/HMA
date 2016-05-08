74517a915e1c320cede5c4dbf01bdc9e64f53bb6
kopawow <kopawow_10>
2016-05-08 22:52:52 +0200

    ds

---
+++ AgataKondraczykLab5/AgataKondraczykLab5/Models/Pizza.cs
@@ -0,0 +1,9 @@
+﻿namespace AgataKondraczykLab5.Models
+{
+    public class Pizza
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Ingredients { get; set; }
+    }
+}
